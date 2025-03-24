20dd7926386f684b0660e3fe88bc1a5ff7981be3
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-03-24 18:50:30 +0000

    chore: cleanup a little

--- build.zig
+++ build.zig
@@ -6,11 +6,17 @@ const DependencyResources = @import("src/build/DependencyResources.zig");
 const ParserResources = @import("src/build/ParserResources.zig");
 const WriteConfigHeader = @import("src/build/WriteConfigHeader.zig");
 
+// TODO: Improve upon this
+// sources:
+// - https://ziggit.dev/docs?topic=3531
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const triple = try target.result.linuxTriple(b.allocator);
-    const sources = try findFilesRecursive(b, "src", &config.cfiles_exts);
+
+    const run_step = b.step("run", "Run the app");
+    const generate_step = b.step("generate", "Generate lexer and parser files");
 
     const exe = b.addExecutable(.{
         .name = b.fmt("{s}-{s}-{s}", .{ config.program_name, triple, @tagName(optimize) }),
@@ -33,13 +39,16 @@ pub fn build(b: *std.Build) !void {
     exe.addIncludePath(b.path("src"));
     exe.addIncludePath(b.path("include"));
     exe.addIncludePath(b.path("generated"));
-    const config_h = b.addConfigHeader(.{
-        .style = .{ .cmake = b.path("include/config.h.in") },
-        .include_path = "config.h",
-    }, .{
-        .tsp_VERSION = "0.1.0",
-        .PROJECT_NAME = "tsp", // TODO: get from config
-    });
+    const config_h = b.addConfigHeader(
+        .{
+            .style = .{ .cmake = b.path("include/config.h.in") },
+            .include_path = "config.h",
+        },
+        .{
+            .tsp_VERSION = "0.1.0",
+            .PROJECT_NAME = "tsp", // TODO: get from config
+        },
+    );
     const write_config_h = WriteConfigHeader.create(b, .{
         .config_header = config_h,
         .output_dir = b.path("include"),
@@ -67,6 +76,7 @@ pub fn build(b: *std.Build) !void {
     dep_resources.install(exe);
 
     // Add source files to the executable
+    const sources = try findFilesRecursive(b, "src", &config.cfiles_exts);
     var all_sources = std.ArrayList([]const u8).init(b.allocator);
     try all_sources.appendSlice(sources);
 
@@ -77,7 +87,6 @@ pub fn build(b: *std.Build) !void {
     });
 
     // Add specific steps to run just flex or bison
-    const generate_step = b.step("generate", "Generate lexer and parser files");
     generate_step.dependOn(parser_resources.step);
     generate_step.dependOn(&write_config_h.step);
 
@@ -90,7 +99,6 @@ pub fn build(b: *std.Build) !void {
             run_cmd.addArgs(args);
         }
 
-        const run_step = b.step("run", "Run the app");
         run_step.dependOn(&run_cmd.step);
     }
 }
